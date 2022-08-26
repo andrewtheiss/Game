1f1e9a47f4065aba8bbb3108b3a9ba2494c2e6e4
Andrew Theiss <andrew.theiss@gmail.com>
2022-08-26 08:30:25 -0700

    First commit with new project

---
+++ src/FunGame.java
@@ -0,0 +1,11 @@
+
+public class FunGame {
+	private int money = 0;
+	public FunGame() {
+		money = 1000;
+	}
+	
+	public int getMoney() {
+		return money;
+	}
+}
---
+++ src/FunGameTester.java
@@ -0,0 +1,9 @@
+
+public class FunGameTester {
+
+	public static void main(String[] args) {
+		FunGame fg = new FunGame();
+		System.out.println(fg.getMoney());
+	}
+
+}
