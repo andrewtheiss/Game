52cfe500f1ff1943fd69ce5160994cfd8cbd6d8d
Andrew Theiss <andrew.theiss@gmail.com>
2022-08-26 08:40:17 -0700

    Added life to the game

--- src/FunGame.java
+++ src/FunGame.java
@@ -1,6 +1,7 @@
 
 public class FunGame {
 	private int money = 0;
+	private int life = 100;
 	public FunGame() {
 		money = 1000;
 	}
--- src/FunGameTester.java
+++ src/FunGameTester.java
@@ -4,6 +4,7 @@ public class FunGameTester {
 	public static void main(String[] args) {
 		FunGame fg = new FunGame();
 		System.out.println(fg.getMoney());
+		System.out.println(fg.getMoney());
 	}
 
 }
